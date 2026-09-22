9944dad9ce5541b1b3a61ad2f79de44bf49caee0
wlwlwwlwllwlw-boop <wlwlwwlwllwlw@gmail.com>
2026-09-23 01:44:57 +0700

    Add files via upload

---
+++ deepseek_lua_20260922_59b136.lua
@@ -0,0 +1,11 @@
+local DELAY = {
+    afterTP      = 0.8,      -- increased
+    insertPotato = 3.0,      -- increased
+    cookProcess  = 3.0,      -- increased
+    insertFlour  = 3.0,      -- increased
+    potOpen      = 1.2,      -- increased
+    claimChips   = 2.5,      -- increased
+    panasinWait  = 2.5,      -- increased
+    sellWait     = 0.4,
+    cookTime     = 60,
+}
