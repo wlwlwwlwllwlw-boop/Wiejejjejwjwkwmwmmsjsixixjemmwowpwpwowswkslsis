fc4aaf2ded4de6689ab6a28310beb0ebb0534a73
wlwlwwlwllwlw-boop <wlwlwwlwllwlw@gmail.com>
2026-09-26 22:32:53 +0700

    Add files via upload

---
+++ v4_bootstrapper_marbeg.dump.lua
@@ -0,0 +1,7 @@
+-- fuhed at OmniDBF .gg/odbf
+
+task.spawn(function() end --[[ callback ]]) --[[ x2 ]]
+task.delay(function() end --[[ callback ]])
+Instance.new("ScreenGui")
+Instance.new("Frame")
+Instance.new("Frame").Position = function() end --[[ callback ]]
